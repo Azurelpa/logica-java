23a8247ef100fccc464a1b71247a1e7e2ff6ee00
Entra21 <entra21@proway.treina>
2021-05-07 18:51:27 -0300

    calculadora IMC

---
+++ src/CalculadoraImc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalculadoraImc {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        // ENTRADA DE DADOS
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu peso: ");
+        double peso = scanner.nextDouble();
+
+        // PROCESSAMENTO
+        double imc = peso/altura/altura;
+        String valoresConcatenados = nome+", seu IMC é "+imc;
+
+        // SAIDA DE DADOS
+        System.out.println(valoresConcatenados);
+        scanner.close();
+    }
+
+}

7a3c7777cf21fe77df7dd941c7082aff00d8854d
Entra21 <entra21@proway.treina>
2021-05-07 21:14:48 -0300

    Metodos de formatação

---
+++ src/Formatacao.java
@@ -0,0 +1,15 @@
+public class Formatacao {
+    public static void main(String[] args) {
+
+        double x = 10.35784;
+        // imprime o texto SEM uma nova linha
+        System.out.print(x);
+
+        // imprime o texto COM uma nova linha;
+        System.out.println(x);
+
+        // imprime o valor e formata a saída
+        System.out.printf("%.2f%n", x);
+        System.out.printf("%.2f%n", x)
+    }
+}

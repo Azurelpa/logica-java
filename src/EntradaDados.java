79b983ddbfbd754ddbdb35d6eacd5a6e8a48412b
Entra21 <entra21@proway.treina>
2021-05-12 19:45:34 -0300

    Resolvido problema

--- src/EntradaDados.java
+++ src/EntradaDados.java
@@ -1,12 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class EntradaDados {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
         int x;
         String s1,s2,s3;
-
-        x = sc.nextInt();
-        s1 = sc.nextLine();
-        s2 = sc.nextLine();
-        s3 = sc.nextLine();
+        x = scanner.nextInt();
+        scanner.nextLine();
+        s1 = scanner.nextLine();
+        s2 = scanner.nextLine();
+        s3 = scanner.nextLine();
 
         System.out.println("DADOS DIGITADOS: ");
         System.out.println(x);

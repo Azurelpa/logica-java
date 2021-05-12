ad34329b6422d90503093937db6a4b641bd1b9af
Entra21 <entra21@proway.treina>
2021-05-12 20:52:12 -0300

    Bhaskara em Java

---
+++ src/FuncaoBaskhara.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FuncaoBaskhara {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor do seu A");
+        double a = scanner.nextDouble();
+
+        System.out.println("Digite o valor do seu B");
+        double b = scanner.nextDouble();
+
+        System.out.println("Digite o valor do C");
+        double c = scanner.nextDouble();
+
+        double delta = Math.pow(b, 2.0)-4*a*c;
+        System.out.println("Valor de delta é " + delta);
+
+        double x1 = (-b + Math.sqrt(delta))/(2.0*a);
+        System.out.println("Valor de X1 é " + x1);
+
+        double x2 = (-b - Math.sqrt(delta))/(2.0*a);
+        System.out.println("Valor de X2 é "+x2);
+
+        scanner.close();
+
+    }
+}

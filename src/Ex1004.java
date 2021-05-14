8726d7a9ce83c3bbbfea7cbfb18baa8de40a1cee
Entra21 <entra21@proway.treina>
2021-05-14 18:28:43 -0300

    Exercício Resolvido

---
+++ src/Ex1004.java
@@ -0,0 +1,34 @@
+/*
+Leia dois valores inteiros.
+A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+A seguir mostre a variável PROD com mensagem correspondente.
+
+Exemplos de Entrada	            Exemplos de Saída
+3                                   PROD = 27
+9
+
+-30                                 PROD = -300
+10
+
+
+0                                   PROD = 0
+9
+ */
+import java.util.Scanner;
+import java.util.Locale;
+public class Ex1004 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+        int A = scanner.nextInt();
+
+        System.out.println();
+        int B = scanner.nextInt();
+
+        int PROD = A * B;
+        System.out.println("PROD = " + PROD);
+
+        scanner.close();
+    }
+}

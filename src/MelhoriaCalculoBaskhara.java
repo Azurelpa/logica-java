6f1acea2592f86abe59f25d38a814ca57f20a527
Entra21 <entra21@proway.treina>
2021-05-25 21:35:59 -0300

    Adicionado condicional faltando

--- src/MelhoriaCalculoBaskhara.java
+++ src/MelhoriaCalculoBaskhara.java
@@ -16,15 +16,13 @@ public class MelhoriaCalculoBaskhara {
 
         if(a == 0 || delta < 0.0 ){
             System.out.println("Impossivel de calcular");
-        }
-
-
+        }else{
         double xLinha = (-b + Math.sqrt(delta)) / 2.0 * a;
         double xDuasLinha = (-b - Math.sqrt(delta)) / 2.0 * a;
 
         System.out.printf("X linha = %.5f%n", xLinha);
         System.out.printf("X duas linha = %.5f%n", xDuasLinha);
-
+        }
 
 
         scanner.close();

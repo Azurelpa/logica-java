ee5c8e1df5d58e03d74c5f66c6bfdad8fcc51304
Entra21 <entra21@proway.treina>
2021-05-28 21:01:53 -0300

    Adicionado condicoes com menor

---
+++ src/Menor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Menor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if(a < b && a < c){
+            System.out.println("Menor " + a);
+        }else if (b < c){
+            System.out.println("Menor " + b);
+        }else{
+            System.out.println("Menor " + c);
+        }
+        scanner.close();
+    }
+}

c0f420593604ef8c0bbc603592a01998ea2b0d8f
Your Name <mayconsouzamendes@gmail.com>
2021-06-16 21:11:48 -0300

    Exercicio Resolvido com algumas melhorias

--- src/estruturarepeticao/para/ExUri1067.java
+++ src/estruturarepeticao/para/ExUri1067.java
@@ -6,8 +6,10 @@ public class ExUri1067 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o valor maximo que queira achar os numeros impar");
         int x = scanner.nextInt();
 
+        System.out.println("Os numeros impares sao");
         if (x >= 1 && x <= 1000) {
             for (int i = 0; i <= x; i++) {
                 if (i % 2 != 0) {
